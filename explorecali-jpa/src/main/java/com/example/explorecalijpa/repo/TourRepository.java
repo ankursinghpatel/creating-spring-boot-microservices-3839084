557109db10276108a1866e6450b5a8708cdb39fa
ankursinghpatel <ankur.patel@siemens.com>
2025-06-17 11:27:22 +0000

    done

--- explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
+++ explorecali-jpa/src/main/java/com/example/explorecalijpa/repo/TourRepository.java
@@ -2,7 +2,13 @@ package com.example.explorecalijpa.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+import com.example.explorecalijpa.model.Difficulty;
 import com.example.explorecalijpa.model.Tour;
+import com.example.explorecalijpa.model.TourPackage;
 
 public interface TourRepository extends JpaRepository<Tour, Integer> {
+  List<Tour> findByDifficulty(Difficulty difficulty);
+  List<Tour> findByTourPackageCode(String tourPackageCode);
 }
